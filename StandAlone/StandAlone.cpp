df485daeb6c216548b2c222e4ba0c86fd9bb2d3d
Arcady Goldmints-Orlov <arcady@lunarg.com>
2023-09-01 18:19:46 -0400

    Clean up unused includes.

--- StandAlone/StandAlone.cpp
+++ StandAlone/StandAlone.cpp
@@ -44,11 +44,9 @@
 #include "glslang/Public/ResourceLimits.h"
 #include "Worklist.h"
 #include "DirStackFileIncluder.h"
-#include "./../glslang/Include/ShHandle.h"
 #include "./../glslang/Public/ShaderLang.h"
 #include "../SPIRV/GlslangToSpv.h"
 #include "../SPIRV/GLSL.std.450.h"
-#include "../SPIRV/doc.h"
 #include "../SPIRV/disassemble.h"
 
 #include <cstring>
